1c7dd59e45cb6b42703bb23836d142dad16c85a7
Christian Frausing Brams <christian@brams.dk>
2025-12-07 10:28:21 +0100

    Day 7, addition grid stuff

---
+++ AdventOfCode/Day07.cs
@@ -0,0 +1,90 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    internal class Day07 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day07()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var grid = Grid<char>.ParseGrid(_input);
+            var startNodes = grid.GetAllNodes().Where(n => n.Value.Equals('S')).ToList();
+            var currentNodes = new List<GridNode<char>>(startNodes);
+            int splits = 0;
+            while (currentNodes.Count > 0)
+            {
+                var nextNodes = currentNodes.Select(n => grid.GetNeighbour(n.X, n.Y,Direction.StraightDown)).ToList();
+                var splitterNodes = nextNodes.Where(n => n != null && n.Value.Equals('^')).ToList();
+                var nonSplitterNodes = nextNodes.Where(n => n != null && !n.Value.Equals('^')).ToList();
+
+                var splitToNodes = splitterNodes.SelectMany(n => new List<GridNode<char>> { grid.GetNeighbour(n.X, n.Y, Direction.StraightLeft),
+                    grid.GetNeighbour(n.X, n.Y, Direction.StraightRight)
+                }).Where(n => n != null).ToList();
+
+                var allNextNodes = nonSplitterNodes.Union(splitToNodes).ToList();
+                splits = splits + splitterNodes.Count;
+                currentNodes = allNextNodes;
+            }
+
+            return new($"Total splits: {splits}");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var grid = Grid<char>.ParseGrid(_input);
+            var valueGrid = grid.TransformGrid(c => Int64.Parse("0")); //Why is this the easiest way to initialize Int64 as zero
+            var startNodes = grid.GetAllNodes().Where(n => n.Value.Equals('S')).ToList();
+            valueGrid.GetNode(startNodes[0].X, startNodes[0].Y).Value = 1;
+            var currentNodes = new List<GridNode<char>>(startNodes);
+            while (currentNodes.Count > 0)
+            {
+                var nextNodes = currentNodes.Select(n => grid.GetNeighbour(n.X, n.Y, Direction.StraightDown)).ToList();
+                var splitterNodes = nextNodes.Where(n => n != null && n.Value.Equals('^')).ToList();
+                var nonSplitterNodes = nextNodes.Where(n => n != null && !n.Value.Equals('^')).ToList();
+
+                foreach (var n in nonSplitterNodes)
+                {
+                    valueGrid.GetNode(n.X, n.Y).Value += valueGrid.GetNeighbour(n.X, n.Y, Direction.StraightUp).Value;
+                }
+
+                var splitLeftNodes = splitterNodes.Select(n => grid.GetNeighbour(n.X, n.Y, Direction.StraightLeft)).Where(n => n != null).ToList();
+                foreach (var n in splitLeftNodes)
+                {
+                    var splitterNode = grid.GetNeighbour(n.X, n.Y, Direction.StraightRight);
+                    var originatorNode = grid.GetNeighbour(splitterNode.X, splitterNode.Y, Direction.StraightUp);
+                    valueGrid.GetNode(n.X, n.Y).Value += valueGrid.GetNode(originatorNode.X,originatorNode.Y).Value;
+                }
+
+                var splitRightNodes = splitterNodes.Select(n => grid.GetNeighbour(n.X, n.Y, Direction.StraightRight)).Where(n => n != null).ToList();
+                foreach (var n in splitRightNodes)
+                {
+                    var splitterNode = grid.GetNeighbour(n.X, n.Y, Direction.StraightLeft);
+                    var originatorNode = grid.GetNeighbour(splitterNode.X, splitterNode.Y, Direction.StraightUp);
+                    valueGrid.GetNode(n.X, n.Y).Value += valueGrid.GetNode(originatorNode.X, originatorNode.Y).Value;
+                }
+
+
+
+
+                var allNextNodes = nonSplitterNodes.Union(splitLeftNodes).Union(splitRightNodes).ToList();
+                currentNodes = allNextNodes;
+            }
+
+            var bottomNodes = valueGrid.GetAllNodes().Where(n => n.Y == valueGrid.Height - 1).ToList();
+
+            return new($"Total timelines: {bottomNodes.Select(bottomNode => bottomNode.Value).Sum()}");
+        }
+
+
+
+    }
+}
--- AdventOfCode/Utils/Grid.cs
+++ AdventOfCode/Utils/Grid.cs
@@ -22,6 +22,20 @@ namespace AdventOfCode.Utils
 
         public GridNode<T> GetNode(int x, int y)
         {
+            int newX = x;
+            int newY = y;
+            if (wrap)
+            {
+                if (newX < 0) newX = nodes.GetLength(0) - 1;
+                if (newX >= nodes.GetLength(0)) newX = 0;
+                if (newY < 0) newY = nodes.GetLength(1) - 1;
+                if (newY >= nodes.GetLength(1)) newY = 0;
+                return nodes[newX, newY];
+            }
+            if (x < 0 || x >= nodes.GetLength(0) || y < 0 || y >= nodes.GetLength(1))
+            {
+                return null;
+            }
             return nodes[x, y];
         }
 
@@ -33,19 +47,10 @@ namespace AdventOfCode.Utils
                 {
                     if(Math.Abs(i) != Math.Abs(j))
                     {
-                        int newX = x + i;
-                        int newY = y + j;
-                        if(newX >= 0 && newX < nodes.GetLength(0) && newY >= 0 && newY < nodes.GetLength(1))
+                        var node = GetNode(x+i, y+j);
+                        if(node != null)
                         {
-                            yield return nodes[newX, newY];
-                        }
-                        if(wrap)
-                        {
-                            if(newX < 0) newX = nodes.GetLength(0) - 1;
-                            if(newX >= nodes.GetLength(0)) newX = 0;
-                            if(newY < 0) newY = nodes.GetLength(1) - 1;
-                            if(newY >= nodes.GetLength(1)) newY = 0;
-                            yield return nodes[newX, newY];
+                            yield return node;
                         }
                     }
                 }
@@ -59,19 +64,10 @@ namespace AdventOfCode.Utils
                 {
                     if (Math.Abs(i) == Math.Abs(j) && i != 0)
                     {
-                        int newX = x + i;
-                        int newY = y + j;
-                        if (newX >= 0 && newX < nodes.GetLength(0) && newY >= 0 && newY < nodes.GetLength(1))
+                        var node = GetNode(x + i, y + j);
+                        if (node != null)
                         {
-                            yield return nodes[newX, newY];
-                        }
-                        if (wrap)
-                        {
-                            if (newX < 0) newX = nodes.GetLength(0) - 1;
-                            if (newX >= nodes.GetLength(0)) newX = 0;
-                            if (newY < 0) newY = nodes.GetLength(1) - 1;
-                            if (newY >= nodes.GetLength(1)) newY = 0;
-                            yield return nodes[newX, newY];
+                            yield return node;
                         }
                     }
                 }
@@ -81,6 +77,31 @@ namespace AdventOfCode.Utils
         public IEnumerable<GridNode<T>> GetAllNeighbours(int x, int y)
         {
             return GetDirectNeighbours(x, y).Concat(GetCrossNeighbours(x, y));
+        }
+
+        public GridNode<T> GetNeighbour(int x, int y, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.StraightUp:
+                    return GetNode(x, y - 1);
+                case Direction.StraightDown:
+                    return GetNode(x, y + 1);
+                case Direction.StraightLeft:
+                    return GetNode(x - 1, y);
+                case Direction.StraightRight:
+                    return GetNode(x + 1, y);
+                case Direction.UpRight:
+                    return GetNode(x + 1, y - 1);
+                case Direction.UpLeft:
+                    return GetNode(x - 1, y - 1);
+                case Direction.DownRight:
+                    return GetNode(x + 1, y + 1);
+                case Direction.DownLeft:
+                    return GetNode(x - 1, y + 1);
+                default:
+                    throw new ArgumentException("Invalid direction");
+            }
         }
 
 
@@ -122,7 +143,27 @@ namespace AdventOfCode.Utils
             }
             return newGrid;
         }
-}
+        public int Width
+        {
+            get { return nodes.GetLength(0); }
+        }
+        public int Height
+        {
+            get { return nodes.GetLength(1); }
+        }
+
+        public void PrintGrid()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Console.Write(nodes[x, y].Value);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
 
     public class GridNode<T>
     {
@@ -137,5 +178,29 @@ namespace AdventOfCode.Utils
         public int X { get; set; }
         public int Y { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return obj is GridNode<T> node &&
+                   EqualityComparer<T>.Default.Equals(Value, node.Value) &&
+                   X == node.X &&
+                   Y == node.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value, X, Y);
+        }
+    }
+
+    public enum Direction
+    {
+        StraightUp,
+        StraightDown,
+        StraightLeft,
+        StraightRight,
+        UpRight,
+        UpLeft,
+        DownRight,
+        DownLeft
     }
 }
